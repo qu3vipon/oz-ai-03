6831c5cd25980c9961daebbd22fa57ba546992f5
qu3vipon <sdh5813@gmail.com>
2026-04-09 14:52:29 +0900

    fastapi

---
+++ database/connection.py
@@ -0,0 +1,26 @@
+# SQLAlchemy를 이용해서 DB와 연결하는 코드
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# 데이터베이스 접속 정보
+DATABASE_URL = "sqlite:///./local.db"
+
+# Engine: DB와 접속을 관리하는 객체
+engine = create_engine(DATABASE_URL, echo=True)
+
+# Session: 한 번의 DB 요청-응답 단위
+SessionFactory = sessionmaker(
+    bind=engine,
+    # 데이터를 어떻게 다룰지를 조정하는 옵션
+    autocommit=False,
+    autoflush=False,
+    expire_on_commit=False,
+)
+
+# SQLAlchemy 세션을 관리하는 함수
+def get_session():
+    session = SessionFactory()
+    try:
+        yield session
+    finally:
+        session.close()
---
+++ user/request.py
@@ -0,0 +1,13 @@
+# 요청 본문의 데이터 형식 관리
+from pydantic import BaseModel, Field
+
+
+# 사용자 추가할 때 데이터 형식
+class UserCreateRequest(BaseModel):
+    name: str = Field(..., min_length=2, max_length=10)
+    job: str
+
+
+# 사용자 데이터를 수정할 때 데이터 형식
+class UserUpdateRequest(BaseModel):
+    job: str
